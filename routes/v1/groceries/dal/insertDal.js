aae5291f70663b5ca3726a818d6dac14ed0bdd82
quickbazar <info.quickbaazar@gmail.com>
2021-02-23 14:54:46 +0530

    upload service written as reusable piece

--- routes/v1/groceries/dal/insertDal.js
+++ routes/v1/groceries/dal/insertDal.js
@@ -1,57 +1,7 @@
 const router = require('express').Router();
-const aws = require('aws-sdk')
-const multerS3 = require('multer-s3')
-const multer = require('multer')
-const dotenv = require("dotenv")
-// const { signup } = require("../../../services/auth");
 const { InsertDal } = require("../../../../services/dalServices");
+const { uploadSingle } = require("../../../../services/uploadService");
 
-dotenv.config({ path: "config/.env" })
-
-const s3 = new aws.S3({
-    accessKeyId: `${process.env.AccessKeyId}`,
-    secretAccessKey: `${process.env.SecretAccessKey}`,
-    region: `${process.env.Region}`
-})
-
-const fileFilter = (req, file, cb) => {
-    if (file.mimetype === "image/jpg" ||
-        file.mimetype === "image/jpeg" ||
-        file.mimetype === "image/png") {
-
-        cb(null, true);
-    } else {
-        cb(new Error("Image uploaded is not of type jpg/jpeg or png"), false);
-    }
-}
-var upload = multer({
-    storage: multerS3({
-        s3: s3,
-        bucket: 'quickbazarbinary',
-        acl: 'public-read',
-        metadata: function (req, file, cb) {
-            cb(null, { fieldName: file.fieldname });
-        },
-        key: function (req, file, cb) {
-            cb(null, file.originalname)
-        }
-    })
-})
-
-var uploadImg = multer({
-    fileFilter,
-    storage: multerS3({
-        s3: s3,
-        bucket: 'ozochat/Replica',
-        acl: 'public-read',
-        metadata: function (req, file, cb) {
-            cb(null, { fieldName: file.fieldname });
-        },
-        key: function (req, file, cb) {
-            cb(null, file.originalname)
-        }
-    })
-})
 
 /**
  * @swagger
@@ -68,6 +18,6 @@ var uploadImg = multer({
  *        price:req.body.price
  *  description:a
  */
-router.post("/insertDal", upload.single('image'), InsertDal);
+router.post("/insertDal", uploadSingle.single('image'), InsertDal);
 
 module.exports = router
